0e7e58a89a5bd99ed6888d53b1ca257bac1cb883
rald <rald@users.noreply.github.com>
2016-07-29 09:34:07 +0800

    Add files via upload

--- pb.c
+++ pb.c
@@ -66,15 +66,15 @@ int main(int argc,char **argv) {
 		switch(c[cp]) {
 			case '+': m[mp]++; break;
 			case '-': m[mp]--; break;
-			case '<': mp--; if(mp<0) { getpos(cp); printf("error memory ln %lu col %lu: pointer below MIN\n",ln,cl); exit(5); } break;
-			case '>': mp++; if(mp>=MEM_MAX) { getpos(cp); printf("error ln %lu col %lu: memory pointer above MAX\n",ln,cl); exit(6); } break;
+			case '<': mp--; if(mp<0) { getpos(cp); printf("error memory ln %lu col %lu: pointer below MIN\n",(unsigned long)ln,(unsigned long)cl); exit(5); } break;
+			case '>': mp++; if(mp>=MEM_MAX) { getpos(cp); printf("error ln %lu col %lu: memory pointer above MAX\n",(unsigned long)ln,(unsigned long)cl); exit(6); } break;
 			case '.': putchar(m[mp]); break;
 			case ',': m[mp]=((ch=getchar())==EOF?0:ch); break;
 			case '[':
 				scp=cp;
 				while(d!=0) {
 					cp++;
-					if(cp>=n) { getpos(scp); printf("error ln %lu col %lu: unmatched [\n",ln,cl); exit(7); }
+					if(cp>=n) { getpos(scp); printf("error ln %lu col %lu: unmatched [\n",(unsigned long)ln,(unsigned long)cl); exit(7); }
 					d-=(c[cp]==']')-(c[cp]=='[');
 				}
 				if(m[mp]!=0) {
@@ -85,7 +85,7 @@ int main(int argc,char **argv) {
 				scp=cp;
 				while(d!=0) {
 					cp--;
-					if(cp<0) { getpos(scp); printf("error ln %lu col %lu: unmatched ]\n",ln,cl);  exit(8); }
+					if(cp<0) { getpos(scp); printf("error ln %lu col %lu: unmatched ]\n",(unsigned long)ln,(unsigned long)cl);  exit(8); }
 					d-=(c[cp]=='[')-(c[cp]==']');
 				}
 				if(m[mp]==0) {
@@ -96,7 +96,7 @@ int main(int argc,char **argv) {
 				scp=cp;
 				while(d!=0) {
 					cp++;
-					if(cp>=n) { getpos(scp); printf("error ln %lu col %lu: unmatched (\n",ln,cl); exit(9); }
+					if(cp>=n) { getpos(scp); printf("error ln %lu col %lu: unmatched (\n",(unsigned long)ln,(unsigned long)cl); exit(9); }
 					d-=(c[cp]==')')-(c[cp]=='(');
 				}
 				p[m[mp]]=scp;
@@ -105,16 +105,16 @@ int main(int argc,char **argv) {
 				scp=cp;
 				while(d!=0) {
 					cp--;
-					if(cp<0) { getpos(scp); printf("error ln %lu col %lu: unmatched )\n",ln,cl); exit(10); }
+					if(cp<0) { getpos(scp); printf("error ln %lu col %lu: unmatched )\n",(unsigned long)ln,(unsigned long)cl); exit(10); }
 					d-=(c[cp]=='(')-(c[cp]==')');
 				}
 				cp=scp;
-				if(sp>=0) { cp=s[sp]; sp--; } else { getpos(scp); printf("error ln %lu col %lu: stack is empty\n",ln,cl); exit(11); }
+				if(sp>=0) { cp=s[sp]; sp--; } else { getpos(scp); printf("error ln %lu col %lu: stack is empty\n",(unsigned long)ln,(unsigned long)cl); exit(11); }
 				break;
 			case ':':
 				scp=cp;
-				if(p[m[mp]]==-1) { getpos(scp); printf("error ln %lu col %lu: invalid procedure call %u\n",ln,cl,m[mp]); exit(12); }
-				if(sp<STK_MAX-1) { sp++; s[sp]=cp; cp=p[m[mp]]; } else { getpos(scp); printf("error ln %lu col %lu: stack is full\n",ln,cl); exit(13); }
+				if(p[m[mp]]==-1) { getpos(scp); printf("error ln %lu col %lu: invalid procedure call %u\n",(unsigned long)ln,(unsigned long)cl,(unsigned int)m[mp]); exit(12); }
+				if(sp<STK_MAX-1) { sp++; s[sp]=cp; cp=p[m[mp]]; } else { getpos(scp); printf("error ln %lu col %lu: stack is full\n",(unsigned long)ln,(unsigned long)cl); exit(13); }
 				break;
 			default: break;
 		}
